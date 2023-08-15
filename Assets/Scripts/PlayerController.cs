7bbd5f0340bf95bea16743c011c2cd746a6cbc21
Calvin <bintot914@gmail.com>
2023-08-14 23:33:56 -0400

    Added collisions to stage platforms

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -82,7 +82,7 @@ public class PlayerController : MonoBehaviour
 
     bool IsGrounded()
     {
-        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.53f, 0.15f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.53f, 0.045f), CapsuleDirection2D.Horizontal, 0, groundLayer);
     }
 
     void OnMove(InputValue value)
